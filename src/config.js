1590a6aa7e151c82b1faaf47647cfd52ae1fa085
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-02-24 09:31:10 -0500

    Put that file back where it came from or so help me

---
+++ src/config.js
@@ -0,0 +1,13 @@
+var config = {
+      firebase: {
+            apiKey: "AIzaSyAI_LDdWxQfz0VGrXyJQtaEAGFSSQLuOyw",
+            authDomain: "weatherwear-4e839.firebaseapp.com",
+            databaseURL: "https://weatherwear-4e839.firebaseio.com/",
+            projectId: "weatherwear-4e839",
+            storageBucket: "gs://weatherwear-4e839.appspot.com/",
+            messagingSenderId: "<SENDER_ID>"
+      },
+      maps: {
+            apiKey: 'HDbIfMxNd3Wpp1yzXyWvvWNFsPHFBz2G'
+      }
+};
